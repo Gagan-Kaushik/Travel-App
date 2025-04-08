fd835b77e25701824891e2eadf2c7893f3cfff0c
Gagan-Kaushik <gagankaushik0812@gmail.com>
2025-04-08 08:43:36 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, Input } from "@chakra-ui/react";
+function App() {
+  return(
+  <>
+   
+  
+    <Button colorScheme="teal">Click Me</Button>
+    <Input placeholder="hello  write something"/>
+  
+  </>
+  );
+  }
+
+export default App;
